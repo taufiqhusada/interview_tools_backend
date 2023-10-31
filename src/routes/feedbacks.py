3d40617f9d9372467f42af051958566a4999743d
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2023-10-31 09:49:16 -0400

    add api to do conversation

--- src/routes/feedbacks.py
+++ src/routes/feedbacks.py
@@ -7,14 +7,14 @@ from config.openai_connector import init_openai_config
 feedbacks_bp = Blueprint('feedbacks', __name__)
 
 @feedbacks_bp.route('/feedbacks', methods=['POST'])
-def get_interviews():
+def get_feedback():
     data = request.json
 
     question = data['question']
     transcript = data['transcript']
     comment = data['comment']
 
-    prompt = f"""Your task is to answer a question based on a question, video transcript, and comment  
+    prompt = f"""Your task is to answer a question based on a question, video transcript of an interview, and comment  
                 Question: ```{question}```
                 Transcript: ```{transcript}```
                 Comment: ```{comment}```"""
@@ -30,6 +30,21 @@ def get_interviews():
         "content": prompt
         },
     ],
+    )
+
+    response = response["choices"][0]["message"]["content"]
+    return convert_to_json_resp(response)
+
+
+@feedbacks_bp.route('/feedbacks/conversation', methods=['POST'])
+def do_conversation():
+    data = request.json
+
+    openai = init_openai_config()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=data['messages']
     )
 
     response = response["choices"][0]["message"]["content"]

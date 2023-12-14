2ebf57e9a032a9d648249c621d7b3ecdb2de0e23
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2023-12-14 10:01:22 -0500

    feat: merge multiple audio

--- src/routes/videoProcessor.py
+++ src/routes/videoProcessor.py
@@ -1,37 +1,59 @@
 from flask import request, Blueprint, Response
 from util.response import  convert_to_json_resp
 import subprocess
-
+import tempfile
+import os
+import json
 
 video_processor_bp = Blueprint('video_processor', __name__)
 
 @video_processor_bp.route('/video/merge', methods=['POST'])
 def merge_audio_video():
     try:
-        # Get the uploaded audio and video files from the request
-        audio_file = request.files['audio']
+        # Get the uploaded video file from the request
         video_file = request.files['video']
 
-        # Check if the files are present
-        if not audio_file or not video_file:
-            return convert_to_json_resp({"error": "Both audio and video files are required."}), 400
+        # Check if the video file is present
+        if not video_file:
+            return convert_to_json_resp({"error": "Video file is required."}), 400
 
-        # Save the uploaded files to the server
-        audio_filename = 'audio.mp3'
+        # Save the uploaded video file to the server
         video_filename = 'video.mp4'
-        audio_file.save(audio_filename)
         video_file.save(video_filename)
 
         # Define the output video filename
         output_filename = 'output.mp4'
 
-        # Use FFmpeg to merge audio and video
+        # Create a temporary directory to store the audio files
+        temp_dir = tempfile.mkdtemp()
+
+        # Get the start times from the JSON data
+        start_times = json.loads(request.form.get('start_times'))
+
+        # Merge audio with the video based on start times
+        filter_complex_str = ''
+        audio_inputs = ''
+        for i, audio_file in enumerate(request.files.getlist('audio')):
+            audio_filename = os.path.join(temp_dir, f'audio_{i}.mp3')
+            audio_file.save(audio_filename)
+
+            print(audio_filename)
+
+            start_time = start_times[i]
+            
+            # Delay the audio based on start time
+            filter_complex_str += f'[{i+1}:a]adelay={int(start_time*1000)}|{int(start_time*1000)}[a{i+1}];'
+
+            audio_inputs += f'-i {audio_filename} '
+
+        filter_complex_str += '[0:a]' + ''.join([f'[a{i+1}]' for i in range(len(request.files.getlist('audio')))])
+        filter_complex = f'{filter_complex_str}amix=inputs={len(request.files.getlist("audio")) + 1}:duration=first[aout]'
+
         cmd = [
             'ffmpeg',
             '-i', video_filename,
-            '-i', audio_filename,
-            '-filter_complex',
-            f'[1:a]adelay=5s|5s[a1];[0:a][a1]amix=inputs=2:duration=first[aout]',
+            *audio_inputs.split(),
+            '-filter_complex', filter_complex,
             '-map', '0:v:0',
             '-map', '[aout]',
             '-c:v', 'copy',
@@ -41,14 +63,22 @@ def merge_audio_video():
 
         subprocess.run(cmd, check=True)
 
-        
         # Return the merged video file as a response
         with open(output_filename, 'rb') as video_file:
             response = Response(video_file.read(), content_type='video/mp4')
 
         response.headers['Content-Disposition'] = 'inline; filename=output.mp4'
 
+        # Clean up temporary files and directory
+        for i in range(len(request.files.getlist('audio'))):
+            os.remove(os.path.join(temp_dir, f'audio_{i}.mp3'))
+        os.rmdir(temp_dir)
+
+        os.remove('output.mp4')
+        os.remove('video.mp4')
+
         return response
+
 
     except Exception as e:
         return convert_to_json_resp({"error": str(e)}), 500
